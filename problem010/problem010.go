30986421602b9659bb513bbdae8b155b6bec499e
Adrien Aucher <aucher.adrien@gmail.com>
2018-09-26 17:48:23 +0100

    Add answer to problem #10.

---
+++ problem010/problem010.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func isPrime(n int) bool {
+	switch {
+	case n <= 1:
+		return false
+	case n <= 3:
+		return true
+	case n%2 == 0:
+		return false
+	case n%3 == 0:
+		return false
+	}
+	for i := 5; i*i <= n; i += 6 {
+		if n%i == 0 || n%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// primes returns a slice containing all prime numbers up to limit.
+func primes(limit int) []int {
+	ps := make([]int, 0)
+	for i := 2; i < limit; i++ {
+		if isPrime(i) {
+			ps = append(ps, i)
+		}
+	}
+	return ps
+}
+
+func sum(xs []int) int {
+	sum := 0
+	for _, x := range xs {
+		sum += x
+	}
+	return sum
+}
+
+// Find the sum of all the primes below two million.
+func main() {
+	var limit int
+	flag.IntVar(&limit, "limit", 2000000, "limit of prime summation")
+	flag.Parse()
+
+	ps := primes(limit)
+	fmt.Println(sum(ps))
+}
